2fb835f8f81e56a8b59d8a40deaba91b34df48bf
Manjeet <manjeetkamboj394@gmail.com>
2023-11-08 12:15:48 +0530

    initial commit

---
+++ react-practice/src/App.tsx
@@ -0,0 +1,7 @@
+import Message from "./message";
+
+function App(){
+  return <div><Message name={"Manjeet"}/></div>;
+}
+
+export default App
---
+++ react-practice/src/message.tsx
@@ -0,0 +1,6 @@
+function Message({name="there, Please login"}){
+
+    return <h1>Hello {name}</h1>
+}
+
+export  default Message
